1ac9154b05dbeeb8a3464bd66aa02f88050a5905
dell <dell@DESKTOP-N8EPBS5>
2023-05-28 16:18:39 +0530

    commit second

---
+++ src/DriverClass1.java
@@ -0,0 +1,16 @@
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+
+import CommonFunctionsPackage1.Utility_CommonFunction;
+import TestClassPackage1.Post_TC_2;
+
+public class DriverClass1 {
+	public static void main(String[] args) throws IOException {
+		Post_TC_2.execute();
+		}
+		}
+			
+
+
